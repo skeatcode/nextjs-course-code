9eb3fe32cbc9f6505011cf1c5fe8ea2ed6ae9fa2
skeatcode <k.helm2216@gmail.com>
2023-10-13 12:53:30 -0600

    - fin 40

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,26 +1,26 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 
-import AllMeetupsPage from './pages/AllMeetups';
-import NewMeetupPage from './pages/NewMeetup';
-import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
-        <Route path='/new-meetup'>
+        <Route path="/new-meetup">
           <NewMeetupPage />
         </Route>
-        <Route path='/favorites'>
+        <Route path="/favorites">
           <FavoritesPage />
         </Route>
-        </Switch>
-    </div>
+      </Switch>
+    </Layout>
   );
 }
 
--- react-course/src/components/meetups/MeetupItem.js
+++ react-course/src/components/meetups/MeetupItem.js
@@ -1,19 +1,24 @@
-import classes from './MeetupItem.module.css'
+import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card";
 
 function MeetupItem(props) {
-    return <li className={classes.item}>
+  return (
+    <li className={classes.item}>
+      <Card>
         <div className={classes.image}>
-            <img src={props.image} alt={props.title} />
+          <img src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
-            <h3>{props.title}</h3>
-            <address>{props.address}</address>
-            <p>{props.description}</p>
-            <div className={classes.actions}>
-                <button>To Favorites</button>
-            </div>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+          <div className={classes.actions}>
+            <button>To Favorites</button>
+          </div>
         </div>
-    </li>;
+      </Card>
+    </li>
+  );
 }
 
 export default MeetupItem;
